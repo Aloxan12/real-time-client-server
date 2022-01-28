d37a32268afc8d07e34b3fd1608763e5af0d71b7
Aloxan12 <kvakvaaak@mail.ru>
2022-01-29 02:59:07 +0300

    init GraphQL & Apollo

--- server/jwt_just/index.js
+++ server/jwt_just/index.js
@@ -8,7 +8,7 @@ app.use(express.json)
 
 const start = async ()=>{
     try{
-        await mongoose.connect(`localhost:27017`)
+        await mongoose.connect(`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`)
         app.listen(PORT, ()=>console.log('connect 5000 port'))
     }catch (e) {
         
